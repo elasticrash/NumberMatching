f1e74c233eedd81801c96102e583435ddb55f97c
Stefanos Kouroupis <stefanos.kouroupis@cloudcall.com>
2019-03-20 15:54:31 +0000

    pass hashmap

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,18 +1,30 @@
 extern crate rand;
 use rand::Rng;
+use std::collections::HashMap;
 
 fn main() {
-    println!("Hello, world!");
-    println!("{:?}", random_number_generator());
+    println!("Generating Random Numbers");
+    let generated_numbers = random_number_generator();
+    println!("{:?}", generated_numbers);
+
+    let mut indexed_tokens:HashMap<&String, &String> = HashMap::new();
+
+    for x in 0..generated_numbers.len() {
+        tokenize(generated_numbers[x], &mut indexed_tokens);
+    }
 }
 
 fn random_number_generator() -> Vec<i32> {
     let mut numbers: Vec<i32> = vec![];
     let mut rng = rand::thread_rng();
 
-    for x in 0..100 {
+    for _x in 0..100 {
         numbers.push(rng.gen_range(1000000, 9999999));
     }
 
     return numbers;
+}
+
+fn tokenize(num: i32, index: &mut HashMap<&String, &String>){
+    println!("{:?}", num);
 }
