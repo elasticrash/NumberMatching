1587650f7142162912531a92f10eedb9a3232ba4
stefano <s.kouroupis@gmail.com>
2019-03-21 01:37:49 +0000

    stupid

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,19 @@
 extern crate rand;
 use rand::Rng;
 use std::collections::HashMap;
+use std::str::FromStr;
+
+struct Indx {
+    d: HashMap<i32, Indx>,
+    m: Vec<String>,
+}
 
 fn main() {
     println!("Generating Random Numbers");
     let generated_numbers = random_number_generator();
     println!("{:?}", generated_numbers);
 
-    let mut indexed_tokens:HashMap<&String, &String> = HashMap::new();
+    let mut indexed_tokens:HashMap<&i32, &Indx> = HashMap::new();
 
     for x in 0..generated_numbers.len() {
         tokenize(generated_numbers[x], &mut indexed_tokens);
@@ -25,6 +31,23 @@ fn random_number_generator() -> Vec<i32> {
     return numbers;
 }
 
-fn tokenize(num: i32, index: &mut HashMap<&String, &String>){
-    println!("{:?}", num);
+fn tokenize(num: i32, index: &mut HashMap<&i32, &Indx>){
+    let char_vec: Vec<char> = num.to_string().chars().collect();
+
+    for x in 0..char_vec.len(){
+        let digit = char_vec[x];
+        let string_key = digit.to_string();
+        let key = i32::from_str(&string_key).unwrap_or(0);
+        let mut idt:HashMap<&i32, &Indx> = HashMap::new();
+
+        if !idt.contains_key(&key) {
+            
+            let new_index = Indx {
+                d: HashMap::new(),
+                m: vec![]
+            };
+
+            idt.insert(&key, &new_index);
+        }
+    }
 }
